93f35d0ecf481db7c88ed0e42f5e0a4f76e349a6
Morgan Ney <morganney@gmail.com>
2026-05-26 20:06:11 -0500

    feat: dsd defined entry.

--- example/src/dsd/elements/cdn-dynamic-name.js
+++ example/src/dsd/elements/cdn-dynamic-name.js
@@ -5,11 +5,11 @@ export const CdnDynamicName = ({ shadowStyles = '' }) => {
 		<cdn-dynamic-name>
 			${template({ shadowStyles, name: 'cdn-dynamic-name' })}
 			<code slot="registration">&lt;script type="module"&gt;</code>
-			<code slot="registration">&nbsp;&nbsp;import "https://unpkg.com/web-component-best-practices/defined.js?name=cdn-dynamic-name"</code>
+			<code slot="registration">&nbsp;&nbsp;import "https://unpkg.com/web-component-best-practices/dsd/defined.js?name=cdn-dynamic-name"</code>
 			<code slot="registration">&lt;/script&gt;</code>
 			<p>
-				This component demonstrates how to use the side effect entrypoint with a CDN URL and dynamic name via query
-				parameter.
+				This component demonstrates the DSD-specific side effect entrypoint with a CDN URL and dynamic name via
+				query parameter.
 			</p>
 		</cdn-dynamic-name>
 	`
--- example/src/dsd/register.js
+++ example/src/dsd/register.js
@@ -4,4 +4,4 @@ WebComponent.register(WebComponent.tagName)
 WebComponent.register('dynamic-name')
 WebComponent.register('no-side-effects')
 
-await import(/* @vite-ignore */ 'https://unpkg.com/web-component-best-practices/defined.js?name=cdn-dynamic-name')
+await import(/* @vite-ignore */ 'https://unpkg.com/web-component-best-practices/dsd/defined.js?name=cdn-dynamic-name')
