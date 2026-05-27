8fd50f6da3101aa582bcd47fe761023ad39a389b
Morgan Ney <morganney@gmail.com>
2026-05-26 19:35:38 -0500

    refactor: address pr comments.

--- example/src/dsd/elements/dynamic-name.js
+++ example/src/dsd/elements/dynamic-name.js
@@ -5,11 +5,11 @@ export const DynamicName = ({ shadowStyles = '' }) => {
 		<dynamic-name>
 			${template({ shadowStyles, name: 'dynamic-name' })}
 			<code slot="registration">&lt;script type="module"&gt;</code>
-			<code slot="registration">&nbsp;&nbsp;import './src/defined.js?name=dynamic-name'</code>
+			<code slot="registration">&nbsp;&nbsp;import WebComponent from './src/element.js'</code>
+			<code slot="registration">&nbsp;&nbsp;WebComponent.register('dynamic-name')</code>
 			<code slot="registration">&lt;/script&gt;</code>
 			<p>
-				This component is registered with a custom tag name via query parameter while using the side effect
-				entrypoint.
+				This component is registered with a custom tag name via explicit register('dynamic-name').
 			</p>
 		</dynamic-name>
 	`
--- example/src/dsd/elements/web-component-best-practices.js
+++ example/src/dsd/elements/web-component-best-practices.js
@@ -5,9 +5,10 @@ export const WebComponentBestPractices = ({ shadowStyles = '' }) => {
     <web-component-best-practices>
       ${template({ shadowStyles })}
       <code slot="registration">&lt;script type="module"&gt;</code>
-      <code slot="registration">&nbsp;&nbsp;import './src/defined.js' // Uses default name</code>
+      <code slot="registration">&nbsp;&nbsp;import WebComponent from './src/element.js'</code>
+      <code slot="registration">&nbsp;&nbsp;WebComponent.register(WebComponent.tagName) // Default name</code>
       <code slot="registration">&lt;/script&gt;</code>
-      <p>This component is registered as a side effect of importing the module from the './defined.js' entrypoint.</p>
+      <p>This component uses explicit registration with the default tag name.</p>
     </web-component-best-practices>
   `
 }
